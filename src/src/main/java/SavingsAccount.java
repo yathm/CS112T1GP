d2db7e40355c86360c56787daf70111ea6cd3bfe
Alisha <alisha.attigan.2023@uni.strath.ac.uk>
2024-03-25 11:26:11 +0000

    added code to savings account class and created test cases for savings account, amended bank account to static voids

---
+++ src/src/main/java/SavingsAccount.java
@@ -0,0 +1,34 @@
+public class SavingsAccount extends BankAccount {
+    //initialise variables
+    private float rate;
+
+    public SavingsAccount(String accountName, String accountID, float rate) {
+        super(accountName, accountID);
+        setRate(rate);
+    }
+
+    //getter to get rate
+    public float getRate() {
+        return rate;
+    }
+    //setter to set rates
+public void setRate(float rate) {
+        this.rate = rate;
+    }
+
+    // method to add interest to current balance
+    public void addInterest() {
+        float interest = getBalance() * (rate / 100);
+        float newBalance = getBalance() + interest;
+        setBalance(newBalance);
+    }
+
+    //method to print account details
+    public void printDetails() {
+        System.out.println("Account Name: " + getAccountName());
+        System.out.println("Account id: " + getAccountNumber());
+        System.out.println("Balance: " + getBalance());
+        System.out.println("Account type: Savings");
+        System.out.println("Rate: " + rate + "%");
+    }
+}
---
+++ src/src/test/java/SavingsAccountTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SavingsAccountTest {
+    //test to make sure set rate method works
+        @Test
+        public void testSetRate() {
+            SavingsAccount savingsAccount = new SavingsAccount("Test Account", "123456", 3.5F);
+            savingsAccount.setHighRate(5);
+            savingsAccount.setRate(3.5F);
+            assertEquals(3.5, savingsAccount.getRate());
+            savingsAccount.setLowRate(2);
+            assertEquals(2, BankAccount.getLowRate());
+            savingsAccount.setHighRate(10);
+            assertEquals(10, BankAccount.getHighRate());
+        }
+        //test to make sure interest is added to balance
+        @Test
+        public void testAddInterest() {
+            SavingsAccount savingsAccount = new SavingsAccount("Test Account", "123456", 5.0F);
+            savingsAccount.setBalance(100.0F);
+            savingsAccount.addInterest();
+            assertEquals(105.0, savingsAccount.getBalance());
+        }
+    }
